75e320e5924d2268edb41f0db548c78218463b9a
JerryWang <495388884@qq.com>
2019-06-11 23:04:13 +0800

    practice ran sendError: etc

--- RACStudyProject/ViewController.m
+++ RACStudyProject/ViewController.m
@@ -9,10 +9,21 @@
 #import "ViewController.h"
 #import <ReactiveObjC.h>
 
+typedef NS_ENUM(NSUInteger, AccessTwitterResultType) {
+    AccessTwitterResultTypeSuccess,
+    AccessTwitterResultTypeRefused,
+    AccessTwitterResultTypeError,
+};
+
+
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *accountField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *accessBtn;
+@property (nonatomic,assign) NSInteger clickCount;
 
 @end
 
@@ -21,7 +32,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.title = @"RACLogin";
+    self.title = @"RACStudy";
+    
+    [self studyDemoOne_login];
+    [self studyDemoTwo_accessTwitter];
+    
+}
+
+- (void)studyDemoTwo_accessTwitter{
+    
+    RACSignal *resultSignal = [[self.accessBtn rac_signalForControlEvents:UIControlEventTouchUpInside] flattenMap:^__kindof RACSignal * _Nullable(__kindof UIControl * _Nullable value) {
+        return [self requestToAccessTwitterAccount];
+    }];
+    
+    RAC(self.stateLabel,text,@"Nothing now!") = resultSignal;
+
+}
+
+- (RACSignal *)requestToAccessTwitterAccount{
+    
+    return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+        
+        switch (self.clickCount++ % 3) {
+                
+            case AccessTwitterResultTypeSuccess:
+                [subscriber sendNext:@"Access success!"];
+                break;
+                
+            case AccessTwitterResultTypeRefused:
+//                [subscriber sendError:[NSError errorWithDomain:@"AccessDomain" code:0 userInfo:@{NSLocalizedDescriptionKey:@"Access refused!"}]];
+                [subscriber sendNext:@"Access refused!"];
+                break;
+            case AccessTwitterResultTypeError:
+//                [subscriber sendError:[NSError errorWithDomain:@"AccessDomain" code:0 userInfo:@{NSLocalizedDescriptionKey:@"An error occurred!"}]];
+                [subscriber sendNext:@"Access error!"];
+                break;
+        }
+        
+        [subscriber sendCompleted];
+        return nil;
+    }];
+}
+
+- (void)studyDemoOne_login{
     
     // rac signal
     
@@ -59,7 +112,6 @@
         @strongify(self);
         self.loginBtn.enabled = NO;
     }] flattenMap:^__kindof RACSignal * _Nullable(__kindof UIControl * _Nullable value) {
-        @strongify(self);
         return [self signInSignal];
     }] subscribeNext:^(NSNumber *success) {
         @strongify(self);
@@ -69,6 +121,11 @@
         }
     }];
 }
+
+
+#pragma mark - private method for demos
+
+// create login callback infomation signal
 
 - (RACSignal *)signInSignal{
     return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
